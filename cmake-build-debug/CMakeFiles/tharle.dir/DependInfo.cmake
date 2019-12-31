54cccd3c39ba5855932e0eab57c60461bf615032
TuffleberryHarle <ca7chy@ya.ru>
2019-12-31 21:07:45 +0300

    hooita

---
+++ cmake-build-debug/CMakeFiles/tharle.dir/DependInfo.cmake
@@ -0,0 +1,91 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_any.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_any.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_atoi.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_atoi.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_bzero.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_bzero.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_count_if.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_count_if.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_create_elem.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_create_elem.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_isalnum.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_isalnum.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_isalpha.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_isalpha.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_isascii.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_isascii.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_isdigit.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_isdigit.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_isprint.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_isprint.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_itoa.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_itoa.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_list_at.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_list_at.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_list_push_back.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_list_push_back.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_lstadd.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_lstadd.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_lstdel.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_lstdel.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_lstdelone.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_lstdelone.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_lstiter.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_lstiter.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_lstmap.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_lstmap.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_lstnew.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_lstnew.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_memalloc.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_memalloc.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_memccpy.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_memccpy.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_memchr.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_memchr.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_memcmp.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_memcmp.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_memcpy.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_memcpy.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_memdel.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_memdel.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_memmove.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_memmove.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_memset.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_memset.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_putchar.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_putchar.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_putchar_fd.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_putchar_fd.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_putendl.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_putendl.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_putendl_fd.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_putendl_fd.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_putnbr.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_putnbr.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_putnbr_fd.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_putnbr_fd.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_putstr.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_putstr.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_putstr_fd.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_putstr_fd.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_reverse.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_reverse.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strcat.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strcat.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strchr.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strchr.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strclr.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strclr.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strcmp.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strcmp.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strcpy.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strcpy.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strdel.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strdel.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strdup.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strdup.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strequ.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strequ.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_striter.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_striter.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_striteri.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_striteri.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strjoin.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strjoin.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strlcat.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strlcat.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strlen.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strlen.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strmap.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strmap.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strmapi.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strmapi.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strncat.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strncat.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strncmp.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strncmp.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strncpy.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strncpy.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strnequ.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strnequ.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strnew.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strnew.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strnstr.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strnstr.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strrchr.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strrchr.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strsplit.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strsplit.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strstr.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strstr.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strsub.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strsub.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_strtrim.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_strtrim.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_tolower.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_tolower.c.o"
+  "/Users/tharle/CLionProjects/fillit2/libft/ft_toupper.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/libft/ft_toupper.c.o"
+  "/Users/tharle/CLionProjects/fillit2/srcs/add_func.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/srcs/add_func.c.o"
+  "/Users/tharle/CLionProjects/fillit2/srcs/additional_functions.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/srcs/additional_functions.c.o"
+  "/Users/tharle/CLionProjects/fillit2/srcs/get_next_line.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/srcs/get_next_line.c.o"
+  "/Users/tharle/CLionProjects/fillit2/srcs/main.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/srcs/main.c.o"
+  "/Users/tharle/CLionProjects/fillit2/srcs/print_map.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/srcs/print_map.c.o"
+  "/Users/tharle/CLionProjects/fillit2/srcs/solve_tetris.c" "/Users/tharle/CLionProjects/fillit2/cmake-build-debug/CMakeFiles/tharle.dir/srcs/solve_tetris.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../includes"
+  "../libft"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
